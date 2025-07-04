95400b7ffe3c863349583891cb86502e2d68f4cf
Nicholo Pardines <nick123abl@gmail.com>
2025-07-04 12:40:57 +0800

    constructed test

--- src/main/java/org/example/HmoPayment.java
+++ src/main/java/org/example/HmoPayment.java
@@ -4,6 +4,6 @@ public class HmoPayment implements BillingStrategy{
     @Override
     public double calculateDiscountedBill(double amount){
         System.out.println("Deducted misc. fee and 15% of cost.");
-        return (amount*0.15)-100; //Minus the 100 peso misc. fee and 15%
+        return (amount*0.75)-100; //Minus the 100 peso misc. fee and 15%
     }
 }
---
+++ src/test/java/org/example/BillingContextTest.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BillingContextTest {
+    private final double testBillAmount = 900;
+    private BillingContext billContext;
+
+    @Test
+    void testHmoPayment(){
+        billContext = new BillingContext();
+        billContext.setBillStrategy(new HmoPayment());
+        double actual = billContext.calculateFinalBill(testBillAmount);
+        Assertions.assertEquals(((testBillAmount*0.75)-100),actual);
+    }
+    @Test
+    void testBasePayment(){
+        billContext = new BillingContext();
+        billContext.setBillStrategy(new BasePayment());
+        double actual = billContext.calculateFinalBill(testBillAmount);
+        Assertions.assertEquals((testBillAmount),actual);
+
+    }
+    @Test
+    void testSeniorPayment(){
+        billContext = new BillingContext();
+        billContext.setBillStrategy(new SeniorPayment());
+        double actual = billContext.calculateFinalBill(testBillAmount);
+        Assertions.assertEquals((testBillAmount*0.8),actual);
+    }
+}
