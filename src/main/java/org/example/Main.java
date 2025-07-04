ae829f315381cf5db85db26498fb62e352e243d5
mnljm <jaime.manalo129@gmail.com>
2025-07-04 10:31:53 +0800

    Initial main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,60 @@
 package org.example;
+import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("=== Hospital Billing System ===");
+        System.out.println("1. Register Patient");
+        System.out.println("2. Add Service");
+        System.out.println("3. Compute Bill");
+        System.out.println("4. Exit");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int inChoice = 0;
+        String inName = null;
+        int inID = 0;
+        String inService = null;
+        String inInsurance = null;
+
+
+        while(inChoice != 4){
+            System.out.println("Enter choice: ");
+            inChoice = sc.nextInt();
+            sc.nextLine();
+
+            switch (inChoice){
+                case 1:
+                    System.out.print("Enter Patient Name: ");
+                    inName = sc.nextLine();
+                    System.out.print("Enter Patient ID: ");
+                    inID = sc.nextInt();
+                    sc.next();
+                    //instantiate patient class
+                    //System.out.print("Patient Registered!");
+                    break;
+
+                case 2:
+                    System.out.println("Available Services: ");
+                    // display services offered
+                    System.out.print("Select service to add: ");
+                    inService = sc.nextLine();
+                    //add to service class
+                    //System.out.println("Service added to patient bill.");
+                    break;
+
+                case 3:
+                    System.out.print("Insurance type (hmo/cash/senior): ");
+                    inInsurance = sc.nextLine();
+                    System.out.println("Original cost: "); // add base cost calculation
+                    System.out.println("Discounted cost: ");// add discount calculation
+                    //System.out.println("Bill generated successfully!");
+
+                case 4:
+                    System.out.println("Thank you! Get well soon!");
+
+            }
         }
     }
 }
